aba89d0966681597ca87d8d996d804834fed944a
Paulo Clebson <pauloclebson270993@gmail.com>
2024-03-19 12:32:40 -0300

    feature: edição das pages

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,11 +1,36 @@
 import Head from "../../../components/Head"
 import { SnackTitle } from "../../../components/SnackTitle"
+import { Snacks } from "../../../components/Snacks";
+
+
 
 export default function Burgers() {
+    const data = [
+        {
+            id: 1,
+            snack: 'burguer',
+            name: 'Mega',
+            description:'O artesanal tamanho familia recheado com três carnes suculentas, queijo e bacon.',
+            price: 25.5,
+            image: 'https://i.imgur.com/upjIUnG.jpg',
+        },
+        {
+            id: 2,
+            snack: 'burguer',
+            name: 'Extra Bacon',
+            description:'Criado para os amantes de bacon, possui em todas as suas camadas bacon bem assado e ainda queijo e carne.',
+            price: 23.5,
+            image: 'https://i.imgur.com/B4J04AJ.jpg',
+        }
+    ]
+
+    console.log('data', data);
+
     return(
     <>
         <Head title="Hambúrgueres" />
         <SnackTitle>Hambúrgueres</SnackTitle>
+        <Snacks snacks={data}></Snacks>
     </>
     )
 }
--- src/pages/Main/Pizzas/index.tsx
+++ src/pages/Main/Pizzas/index.tsx
@@ -1,11 +1,32 @@
 import Head from "../../../components/Head"
 import { SnackTitle } from "../../../components/SnackTitle"
+import { Snacks } from "../../../components/Snacks"
 
 export default function Pizzas() {
+    const data = [
+        {
+            id: 1,
+            snack: 'pizza',
+            name: 'Pizza de Calabresa',
+            description:'Deliciosa pizza de calabresa com cebolas',
+            price: 30.99,
+            image: 'https://i.imgur.com/Qtrvqb0.jpeg',
+        },
+        {
+            id: 2,
+            snack: 'burguer',
+            name: 'Extra Bacon',
+            description:'Criado para os amantes de bacon, possui em todas as suas camadas bacon bem assado e ainda queijo e carne.',
+            price: 23.5,
+            image: 'https://i.imgur.com/B4J04AJ.jpg',
+        }
+    ]
+
     return(
         <>
             <Head title="Pizzas" />
             <SnackTitle>Pizzas</SnackTitle>
+            <Snacks snacks={data}></Snacks>
         </>
         )
 }
