d05b97b67a2e7aad6fb34ada163d54b98edeee0d
pauloclebson <pauloclebson270993@gmail.com>
2024-04-25 12:53:02 -0300

    feature: implementando responsividade

--- src/pages/MyCart/Table/index.tsx
+++ src/pages/MyCart/Table/index.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react'
 import { useCart } from '../../../hooks/useCart'
 
 import { TableDesktop } from './TableDesktop'
+import { TableMobile } from './TableMobile'
 
 export function Table(){
+  const [windowWidth, setWindowWidth] = useState(document.documentElement.clientWidth)
+
   const { cart } = useCart()
+
+  useEffect(() => {
+    function updateTablecomponentBaseInWindowWidth(){
+      const currentWidth = document.documentElement.clientWidth
+      setWindowWidth(currentWidth)
+    }
+
+    window.addEventListener('resize', updateTablecomponentBaseInWindowWidth)
+
+    return () => {
+      window.removeEventListener('resize', updateTablecomponentBaseInWindowWidth)
+    }
+  }, [])
 
   if (cart.length === 0 ) return <h1>Ops! Parece que você não tem pedidos, peça já!</h1>
 
-  return <TableDesktop />
+  return windowWidth > 768 ? <TableDesktop /> : <TableMobile />
   }
